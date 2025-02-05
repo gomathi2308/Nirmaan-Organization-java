5bbbee25f0dc4977c51da637b4997e8ced765f1e
gomathi2308 <gomathiu08@gmail.com>
2025-02-05 17:11:06 +0530

    Day9

---
+++ day9/ReverseNum.java
@@ -0,0 +1,12 @@
+package day9;
+
+public class ReverseNum {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=50; i>=1; i--) {
+			System.out.println(i);
+	   } 
+	}
+
+}
---
+++ day9/ThreeDivisible.java
@@ -0,0 +1,15 @@
+package day9;
+
+public class ThreeDivisible {
+
+	public static void main(String[] args) {
+		int count=0;
+		for(int i=1; i<=30; i++) {
+        	if(i%3==0) {
+        		System.out.println(i);
+        	
+       }
+	}
+		System.out.println("count of odd numbers:"+count);
+  }
+}
